a732a82ac84068057d18e7d43d8b2163b11a5689
HominSu <hominsu@foxmail.com>
2023-01-04 10:52:29 +0800

    feat(middleware): modified the session middleware
    Signed-off-by: HominSu <hominsu@foxmail.com>

--- app/pallas/service/pkgs/middleware/session.go
+++ app/pallas/service/pkgs/middleware/session.go
@@ -2,9 +2,8 @@ package middleware
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/middleware"
 	"github.com/go-kratos/kratos/v2/transport"
 	"github.com/go-kratos/kratos/v2/transport/http"
@@ -12,15 +11,22 @@ import (
 	"github.com/hominsu/pallas/pkg/sessions"
 )
 
-func Session(store *sessions.RedisStore, name string, helper *log.Helper) middleware.Middleware {
+const (
+	unauthorized string = "UNAUTHORIZED"
+)
+
+var (
+	ErrGetSessionStoreFail = errors.Unauthorized(unauthorized, "get session error")
+)
+
+func Session(store *sessions.RedisStore, name string) middleware.Middleware {
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
 			if tr, ok := transport.FromServerContext(ctx); ok {
 				if ht, ok := tr.(*http.Transport); ok {
 					session, err := store.Get(ht, name)
 					if err != nil {
-						helper.Error(fmt.Sprintf("get session error: %v", err))
-						return handler(ctx, req)
+						return nil, ErrGetSessionStoreFail
 					}
 					if id, ok := session.Values["userid"]; ok {
 						if userId, ok := id.(int64); ok {
